aa9783e4b267ae6d10d2cea55be5ebb509e31594
Slava <1760002187401.sn@gmail.com>
2019-11-07 23:37:02 +0300

    test2

---
+++ tests.py
@@ -0,0 +1,10 @@
+import datetime
+def main()
+    ##DATE OBJECTS
+#Get today's date from the datetime class
+today = datetime.now()
+print("the current datetime is",today)
+if __name__=="__main__":
+main()
+
+
