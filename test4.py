3b6eea0a3ee7709a0ae5c8d434446a7be2c725f5
Slava <1760002187401.sn@gmail.com>
2019-12-02 20:43:30 +0300

    test4

--- test4.py
+++ test4.py
@@ -1,13 +1,30 @@
-def input(n):
-# проверяем является ли число целым
-    n = input("Введите число: ")
+from test3 import is_year_leap
 
-while type(n) != int:
-    try:
-        n = int(n)
-    except ValueError:
-        print("неправильно ввели!")
-        n = input("Введите целое число: ")
+def input_year():
+    # проверяем является ли число целым
+    while True:
+        try:
+            n = int(input("Введите год: "))
+            if n > 0:
+                if n > 2050:
+                    print('max 2050')
+                    continue
+                return n
+        except:
+            print("ошибка")
+
+def main():
+    while True:
+        year = input_year()
+        if is_year_leap(year):
+            print('високосный')
+        else:
+            print('невисокосный')
+        answer = input('Хотите еще проверить (y/n)')
+        if answer == 'n':
+            break
 
 
+if __name__ == '__main__':
+    main()
 
